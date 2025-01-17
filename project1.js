6804287d20ea349fd913af2352cf4b83f7636798
16808910041680 <forforfirefire@gmail.com>
2025-01-17 11:10:45 +1100

    program123.html

---
+++ project1.js
@@ -0,0 +1,89 @@
+var check1 = false
+var check2 = false
+var check3 = false
+var check4 = false
+var check5 = false
+function onclick1() {
+  if (check1 == false) {
+      check1 = true
+      if (check1 && check2 && check3 && check4 && check5 == true) {
+          alert("Acheivement: Randomly Press Buttons")
+          check1 = false
+          check2 = false
+          check3 = false
+          check4 = false
+          check5 = false
+      }
+  }
+  
+  else {
+    check1 = false
+  }
+}
+function onclick2() {
+    if (check2 == false) {
+        check2 = true
+        if (check1 && check2 && check3 && check4 && check5 == true) {
+            alert("Acheivement: Randomly Press Buttons")
+            check1 = false
+            check2 = false
+            check3 = false
+            check4 = false
+            check5 = false
+        }
+    }
+   
+    else {
+        check2 = false
+    }
+}
+function onclick3() {
+    if (check3 == false) {
+         check3 = true
+        if (check1 && check2 && check3 && check4 && check5 == true) {
+            alert("Acheivement: Randomly Press Buttons")
+            check1 = false
+            check2 = false
+            check3 = false
+            check4 = false
+            check5 = false
+        }
+    }
+    
+    else {
+        check3 = false
+    }
+}
+function onclick4() {
+    if (check4 == false) {
+         check4 = true
+        if (check1 && check2 && check3 && check4 && check5 == true) {
+            alert("Acheivement: Randomly Press Buttons")
+            check1 = false
+            check2 = false
+            check3 = false
+            check4 = false
+            check5 = false
+        }
+    }
+    else {
+        check4 = false
+    }
+}
+function onclick5() {
+    if (check5 == false) {
+         check5 = true
+        if (check1 && check2 && check3 && check4 && check5 == true) {
+            alert("Acheivement: Randomly Press Buttons")
+            check1 = false
+            check2 = false
+            check3 = false
+            check4 = false
+            check5 = false
+        }
+    }
+
+    else {
+         check5 = false
+    }
+}
